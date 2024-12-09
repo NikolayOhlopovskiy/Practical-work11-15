3326989a10dc3a9b3702acce4a785b7925c059cc
NikolayOhlopovskiy <nik.okhlopovskiy.17@gmail.com>
2024-12-09 21:05:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            Array.Reverse(array);
+            Console.WriteLine("Реверсированный массив: " + string.Join(", ", array));
+            Console.Read();
+
+        }
+    }
+}
