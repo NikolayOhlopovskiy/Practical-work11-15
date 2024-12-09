83ff89118f710bb1584809532957705983312fa3
NikolayOhlopovskiy <nik.okhlopovskiy.17@gmail.com>
2024-12-09 21:05:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] omas = { "Яблоко", "Ананас", "Киви" };
+            //Array.Reverse(omas);
+            //foreach(var el int revers)
+            //  {
+            //    Console.WriteLine(el);
+            //  }
+            //Array.Sort(omas);
+            Console.Read();
+        }
+    }
+}
