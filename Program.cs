e713003268493cc5a91323e57a70044b45ef3a80
NikolayOhlopovskiy <nik.okhlopovskiy.17@gmail.com>
2024-12-10 10:05:56 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ namespace ConsoleApp15._5
     {
         static void Main(string[] args)
         {
-            int[] array = { 1, 2, 3, 4, 5 };
-            Array.Reverse(array);
+            int[] omas = { 1, 2, 3, 4, 5 };
+            Array.Reverse(omas);
             Console.WriteLine("Реверсированный массив: " + string.Join(", ", array));
             Console.Read();
 
