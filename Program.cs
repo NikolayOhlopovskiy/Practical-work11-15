c4feca78d506279ed36f9aa2de1fb1a766eefb24
NikolayOhlopovskiy <nik.okhlopovskiy.17@gmail.com>
2024-12-10 11:09:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11._8
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Задача 8. Изменение размера массива.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string[] people = { "Tom", "Sam", "Bob", "Kate", "Tom", "Alice" };
+
+            
+            Array.Resize(ref people, 4);
+
+            foreach (var person in people)
+                Console.Write($"{person} ");
+            Console.Read();
+        }
+    }
+}
